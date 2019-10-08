f1238d6152c70ebe63511119d65c4903feff4795
Dusengejeanclaude <dusengejeanclaude@gmail.com>
2019-10-08 14:49:01 +0200

    i have done step 8 of end to end testing

---
+++ spec/features/task.spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.feature "Task management function", type: :feature do
+  
+  scenario "Test task list" do
+
+  Task.create!(name: 'test_task_01', status: 'testtesttest')
+  
+  visit tasks_path
+  save_and_open_page
+
+  expect(page).to have_content'testtesttest'
+   end
+
+  scenario "Test task creation" do
+    visit new_task_path
+    fill_in 'task[name]', :with => 'test'
+    fill_in 'task[status]', :with =>'pending'
+    click_on 'create task'
+    expect(page).to have_content'testtesttest'
+  end
+
+  scenario "Test task details" do
+
+  end
+end
