803033132dbe58e471f6a8219509b88075f80514
Dusengejeanclaude <dusengejeanclaude@gmail.com>
2019-10-09 14:59:59 +0200

    i have made language sharing on step 9

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Everyleaf
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators do |g|
       g.test_framework :rspec,
